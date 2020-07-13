5c82c4dc0180da0f370601a35c8e606ed1979fb1
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-07-13 05:10:51 +0500

    practice

--- src/App.js
+++ src/App.js
@@ -4,21 +4,44 @@ import './App.css';
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+  NavLink,
+  Link,
+  Outlet
 } from 'react-router-dom'
 
 const Home = () => ( <h1>Hi I'am in Home </h1> )
-const Blog = () => ( <h1>Hi I'am in Blog </h1> )
+const BlogIndex = () => {
+  return (
+    <div>
+      <h1> Welcome to our blog</h1>
+
+      <ul>
+        <Link as='li' to='authors'> Authors </Link>
+        <Link as='li' to='1'> Post 1 </Link>
+        <Link as='li' to='2'> Post 2 </Link>
+      </ul>
+
+        <Outlet />
+    </div>
+  )
+}
+
+const BlogAuthors = () => (<h2> Hamza </h2>)
 
 function App() {
   return (
     <Router>
-      <div className="App">
-      <h1> This is my project#3 </h1>
-
+      <div>
+        <ul className="navbar">
+           <NavLink to='/' as='li'> Home </NavLink> 
+           <NavLink to='blog' as='li'> Blog </NavLink> 
+        </ul>     
        <Routes>
          <Route path='/' element={<Home />} />
-         <Route path='blog' element={<Blog />} />
+         <Route path='blog' element={<BlogIndex />}>
+           <Route path='authors' element={<BlogAuthors />} />
+         </Route>
        </Routes>
      </div>
     </Router>
