0e4dad97fa57ccaf30fc92efee075f82148f10d3
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-12-27 20:56:19 +0500

    Update Header.js

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
- import Menu from '../svg/bars-solid.svg'
+import Menu from '../svg/bars-solid.svg'
 import Close from '../svg/times-solid.svg'
 import CartIcon from '../svg/shopping-cart-solid.svg'
 import {Link} from 'react-router-dom'
