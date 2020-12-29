00bb73dd68eafee85e65782f43cba88965038f92
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-12-29 06:45:15 +0500

    maybe I solved an error in cart.js.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,64 +1,63 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { DataContext } from './DataProvider';
-// import Colors from './Colors';
-// import Sizes from './Sizes';
-// import { Link } from 'react-router-dom'; useState, useEffect
+import Colors from './Colors';
+import Sizes from './Sizes';
+import { Link } from 'react-router-dom'; 
 
 export default function Cart() {  
    const value = useContext(DataContext) 
-  //  const [cart, setCart] = value.cart
-  //  const [total, setTotal] = useState(0);   
+   const [cart, setCart] = value.cart
+   const [total, setTotal] = useState(0);   
 
    console.log(value);
 
-//    useEffect(() => {
-//      const getTotal = () => {
-//        const res = cart.reduce((prev, item) => {
-//          return prev + item.price * item.count;
-//        }, 0);
-//        setTotal(res);
-//      };
-//      getTotal();
-//    }, [cart]);
+   useEffect(() => {
+     const getTotal = () => {
+       const res = cart.reduce((prev, item) => {
+         return prev + item.price * item.count;
+       }, 0);
+       setTotal(res);
+     };
+     getTotal();
+   }, [cart]);
 
-//     const increase = (id) => {
-//      cart.forEach((item) => {
-//        if (item._id === id) {
-//          item.count += 1;
-//         }
-//       });
-//       setCart([...cart]);
-//     };
+    const increase = (id) => {
+     cart.forEach((item) => {
+       if (item._id === id) {
+         item.count += 1;
+        }
+      });
+      setCart([...cart]);
+    };
 
-//    const reduction = (id) => {
-//      cart.forEach((item) => {
-//        if (item._id === id) {
-//          item.count === 1 ? (item.count = 1) : (item.count -= 1);
-//        }
-//      });
-//      setCart([...cart]);
-//    };
+   const reduction = (id) => {
+     cart.forEach((item) => {
+       if (item._id === id) {
+         item.count === 1 ? (item.count = 1) : (item.count -= 1);
+       }
+     });
+     setCart([...cart]);
+   };
 
    
-//     const removeProduct = id => {
-//       if(window.confirm("Do you want to delete this product?")) {
-//         cart.forEach((item, index) => {
-//           if(item._id === id) {
-//               cart.splice(index, 1)
-//            }
-//         })
-//         setCart([...cart])
-//     }
-// }
+    const removeProduct = id => {
+      if(window.confirm("Do you want to delete this product?")) {
+        cart.forEach((item, index) => {
+          if(item._id === id) {
+              cart.splice(index, 1)
+           }
+        })
+        setCart([...cart])
+    }
+}
 
-//    if(cart.length === 0)
-//      return <h2 style={{textAlign: "center", fontSize: "5rem"}}> Cart Empty </h2>
+   if(cart.length === 0)
+     return <h2 style={{textAlign: "center", fontSize: "5rem"}}> Cart Empty </h2>
 
 
     return (
-      <>
-       <h1>Cart component </h1> 
-       {/* {
+      <>       
+       {
          cart.map(product => (          
           <div className="details cart" key={product._id}>
             <div className="img-container" style={{backgroundImage: `url(${product.images[0]})`}}  />
@@ -86,7 +85,7 @@ export default function Cart() {
         <div className="total">
           <Link to='/payment'> Payment </Link> 
           <h3> Total: ${total} </h3>
-        </div>       */}
+        </div>      
       </>
    )
 }
